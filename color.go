8b7edc053cc31d2966308fdf5976fafa68318d95
mguitar24 <mguitar24@gmail.com>
2025-02-15 00:13:52 -0700

    refactored parsing and added ansi links to print

--- color.go
+++ color.go
@@ -5,9 +5,17 @@ type Color string
 const Blue Color = "\033[34m"
 const Red Color = "\033[31m"
 
-const End Color = "\033[0m"
+const Reset Color = "\033[0m"
+
+const hrefStart = "\033]8;;"
+const hrefEnd = "\033\\"
+const hrefTextEnd = "\033]8;;\033\\"
 
 // adds Unicode color c to message string
-func colorString(c Color, message string) string {
-	return string(c) + message + string(End)
+func colorString(c Color, s string) string {
+	return string(c) + s + string(Reset)
+}
+
+func ansiLink(href string, s string) string {
+	return hrefStart + href + hrefEnd + s + hrefTextEnd
 }
